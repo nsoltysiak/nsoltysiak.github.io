278f9024bb579e65e58cbb1c3ef169d18236fbf2
nsoltysiak <nsoltysiak@pm.me>
2025-10-08 12:26:10 -0400

    index.js update

--- Major Project/index.js
+++ Major Project/index.js
@@ -11,12 +11,18 @@ for (let i = 0; i < navLinks.length; i++) {
 // Select the dropdown element
 const dropdown = document.querySelector(".dropdown");
 
+function addShowClass()
+{
+    dropdown.classList.add("show");
+}
+
+function removeShowClass()
+{
+  dropdown.classList.remove("show");
+}
+
 // When mouse enters, show the dropdown
-dropdown.addEventListener("mouseenter", () => {
-  dropdown.classList.add("show");
-});
+dropdown.addEventListener("mouseenter", addShowClass);
 
 // When mouse leaves, hide the dropdown
-dropdown.addEventListener("mouseleave", () => {
-  dropdown.classList.remove("show");
-});
+dropdown.addEventListener("mouseleave", removeShowClass);
